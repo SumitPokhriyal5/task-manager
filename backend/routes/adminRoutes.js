24abb934e76aff9bc2ac886244f4e1d596e5a02a
Sumit Pokhriyal <sumitstan1998@gmail.com>
2026-06-10 13:55:49 +0530

    add admin analytics stats api

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -6,12 +6,15 @@ import {
   getAllTasks,
   deleteAnyTask,
   getActivityLogs,
+  getStats,
 } from "../controllers/adminController.js";
 import { protect, adminOnly } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.use(protect, adminOnly);
+
+router.get("/stats", getStats);
 
 router.get("/users", getAllUsers);
 router.delete("/users/:id", deleteUser);
